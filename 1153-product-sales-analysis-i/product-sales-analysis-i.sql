6a399015464c45d3a2e820f7293f5bbee3c25e62
Walid Rashad <143013141+walidrashadx@users.noreply.github.com>
2025-08-12 02:17:52 +0300

    Time: 709 ms (86.53%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -0,0 +1,13 @@
+/* Write your PL/SQL query statement below */
+select 
+pt.product_name, 
+s.year ,
+
+  s.price 
+
+
+  from 
+
+  product pt inner join Sales  s on pt.product_id  =s.product_id 
+
+
