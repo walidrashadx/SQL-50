6cd52ff29d3b9f5c18cdb5657c1f33bd0a1d5cb1
Walid Rashad <143013141+walidrashadx@users.noreply.github.com>
2025-08-16 23:58:39 +0300

    Time: 711 ms (57.76%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,11 @@
+/* Write your PL/SQL query statement below */
+SELECT s.user_id,
+       ROUND(
+         NVL(SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) 
+         / NULLIF(COUNT(c.action), 0), 0)
+       , 2) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+       ON s.user_id = c.user_id
+GROUP BY s.user_id
+ORDER BY s.user_id;
