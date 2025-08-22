f4413e45eb13c5beb65a009b262f664ec67f8800
Walid Rashad <143013141+walidrashadx@users.noreply.github.com>
2025-08-22 23:56:07 +0300

    Time: 409 ms (77.18%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,7 @@
+SELECT
+  query_name,
+  ROUND(AVG(rating * 1.0 / position), 2)          AS quality,
+  ROUND(100.0 * AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END), 2)
+                                                   AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
