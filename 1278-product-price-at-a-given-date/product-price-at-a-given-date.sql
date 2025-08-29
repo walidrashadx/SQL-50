3f6d232534fca565c33403fca60e47bd53f57a47
Walid Rashad <143013141+walidrashadx@users.noreply.github.com>
2025-08-29 21:05:20 +0300

    Time: 569 ms (48.77%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,15 @@
+SELECT p1.product_id,
+       COALESCE(p2.new_price, 10) AS price
+FROM (SELECT DISTINCT product_id FROM Products) p1
+LEFT JOIN (
+    SELECT product_id,
+           new_price,
+           ROW_NUMBER() OVER (
+               PARTITION BY product_id
+               ORDER BY change_date DESC
+           ) AS rn
+    FROM Products
+    WHERE change_date <= DATE '2019-08-16'
+) p2
+ON p1.product_id = p2.product_id
+AND p2.rn = 1;
