f502347d9f4399a6c85c80c98b83518a585996f1
Walid Rashad <143013141+walidrashadx@users.noreply.github.com>
2025-08-30 03:50:37 +0300

    Time: 497 ms (87.67%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,6 +1,11 @@
 SELECT p1.product_id,
        COALESCE(p2.new_price, 10) AS price
+       
+       --table
 FROM (SELECT DISTINCT product_id FROM Products) p1
+
+--table
+
 LEFT JOIN (
     SELECT product_id,
            new_price,
@@ -11,5 +16,7 @@ LEFT JOIN (
     FROM Products
     WHERE change_date <= DATE '2019-08-16'
 ) p2
+
+
 ON p1.product_id = p2.product_id
 AND p2.rn = 1;
