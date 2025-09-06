5180461d31e1d5840c7b710c20e8a563f0c426e7
Walid Rashad <143013141+walidrashadx@users.noreply.github.com>
2025-09-07 02:32:16 +0300

    Time: 393 ms (87.55%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,5 +1,5 @@
 WITH AllFriends AS (
-    -- Step 1: Treat friendships as bidirectional
+   -- Treat friendships as bidirectional
     SELECT requester_id AS id, accepter_id AS friend_id
     FROM RequestAccepted
     UNION ALL
@@ -7,14 +7,14 @@ WITH AllFriends AS (
     FROM RequestAccepted
 ),
 FriendCount AS (
-    -- Step 2: Count friends per user
+    --Count friends per user
     SELECT 
         id,
         COUNT(DISTINCT friend_id) AS num
     FROM AllFriends
     GROUP BY id
 )
--- Step 3: Get the person(s) with the maximum friends
+-- Get the person(s) with the maximum friends
 SELECT id, num
 FROM FriendCount
 WHERE num = (SELECT MAX(num) FROM FriendCount);
