d7ee946f411f47c19374cba06459d511402fa4bd
Walid Rashad <143013141+walidrashadx@users.noreply.github.com>
2026-08-31 14:17:49 +0300

    Time: 54 ms (6.32%) | Memory: 53.5 MB (47.49%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,22 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        
+    }
+};
+
+var  createHelloWorld = function (){
+
+    return function(){
+
+        return "Hello World";
+
+    }
+
+};
+
+
+
